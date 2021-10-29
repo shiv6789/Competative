7bf598b22377253e54f580ac6f2b7a2ee4defb01
shiv6789 <54994164+shiv6789@users.noreply.github.com>
2021-10-29 14:59:54 +0530

    Create LongestConsecutiveSequence.java
    https://leetcode.com/problems/longest-consecutive-sequence/submissions/

---
+++ Leetcode/LongestConsecutiveSequence.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        
+        HashSet<Integer> set=new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+         set.add(nums[i]);
+        }
+        
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        for(int i:set){
+            pq.add(i);
+        }
+        if(nums.length==0)
+            return 0;
+            else{
+        int ans=Integer.MIN_VALUE;
+        int max=1;
+        int temp=pq.poll();
+        while(!pq.isEmpty()){
+            int temp1=pq.poll();
+            if(temp1==temp+1){
+                max++;
+                temp=temp1;
+            }
+            else{
+                ans=Math.max(ans,max);
+                temp=temp1;
+                max=1;
+            }
+        }
+        ans=Math.max(ans,max);
+        return ans;
+            }
+
+    }
+}
